a59b2ae76f5ddac4bc73c9dfda684c9bcbf54521
Dzianis Sipakou <d.sipakou@gmail.com>
2018-01-19 15:58:33 +0300

    911 a completed

--- 911/a.py
+++ 911/a.py
@@ -1,12 +1,19 @@
 n = int(input())
 arr = list(map(int, input().split()))
-indexes = [i for i, value in enumerate(arr) if value == min(arr)]
+m = min(arr)
+first = arr.index(m) + 1
+tmp = 1
 output = -1
-for i in range(len(indexes) - 1):
-    if output < 0:
-        output = indexes[i + 1] - indexes[i]
+for i in range(first, len(arr)):
+    if arr[i] == m:
+        if output < 0:
+            output = tmp
+        else:
+            output = min(output, tmp)
+        tmp = 1
     else:
-        output = min(output, indexes[i + 1] - indexes[i])
+        tmp += 1
     if output == 1:
         break
+
 print(output)
