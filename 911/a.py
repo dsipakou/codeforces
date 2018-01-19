bb35fe8572cd7c467e6ff5cbee9442df23cd1212
Dzianis Sipakou <d.sipakou@gmail.com>
2018-01-19 15:46:09 +0300

    2nd try for 911 a

--- 911/a.py
+++ 911/a.py
@@ -1,12 +1,12 @@
 n = int(input())
 arr = list(map(int, input().split()))
+indexes = [i for i, value in enumerate(arr) if value == min(arr)]
 output = -1
-i = arr.count(min(arr))
-for j in range(i - 1):
-    index = arr.index(min(arr))
-    arr = arr[index + 1:]
-    if output > 0:
-        output = min(output, arr.index(min(arr)) + 1)
+for i in range(len(indexes) - 1):
+    if output < 0:
+        output = indexes[i + 1] - indexes[i]
     else:
-        output = arr.index(min(arr)) + 1
+        output = min(output, indexes[i + 1] - indexes[i])
+    if output == 1:
+        break
 print(output)
