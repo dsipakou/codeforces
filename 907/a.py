88546702a068a9d833d0273bd8c7f7e80b01c770
Dzianis Sipakou <d.sipakou@gmail.com>
2018-01-25 13:37:04 +0300

    907 a 2nd ver

--- 907/a.py
+++ 907/a.py
@@ -1,11 +1,11 @@
 v1, v2, v3, vm = map(int, input().split())
-if v1 > v2 > v3:
-    if vm > v3 or 2 * vm < v3:
+if v1 > v2 > max(v3, vm):
+    if 2 * min(v3, vm) < max(vm, v3):
         print('-1')
     else:
         print(v1)
         print(v2)
-        print(v3)
+        print(max(vm, v3))
 
 else:
     print('-1')
