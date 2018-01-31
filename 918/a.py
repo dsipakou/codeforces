91f6c07ca7ea76325ad300f81db198ec37cc17e4
Dzianis Sipakou <d.sipakou@gmail.com>
2018-01-31 13:17:29 +0300

    918 a first try

---
+++ 918/a.py
@@ -0,0 +1,12 @@
+n = int(input())
+s = ''.join(str(s) for s in ['o'] * n)
+arr = [1]
+
+a, b = 1, 1
+
+while b < n:
+    a, b = b, a + b
+    arr.append(b)
+for i in range(len(arr)):
+    s = s[:arr[i] - 1] + 'O' + s[arr[i]:]
+print(s)
